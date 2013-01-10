21d6bb5fda4eaf931c3ccb0e20308639840cc096
Greg Wang <gregwym@qq.com>
2013-01-09 22:28:05 -0500

    Remove bwio.h. Code clean up.

--- train_control_panel.c
+++ train_control_panel.c
@@ -1,5 +1,5 @@
 #include <plio.h>
-#include <bwio.h>
+// #include <bwio.h>
 #include <ts7200.h>
 #include <debug.h>
 
@@ -24,14 +24,12 @@
 
 static unsigned int dbflags;
 
-
-
 /* 
  * Timer Methods
  */
 
-unsigned int setTimerControl(unsigned int* timer_base, unsigned int enable, unsigned int mode, unsigned int clksel) {
-	unsigned int* timer_control_addr = (unsigned int*) ((unsigned int)timer_base + CRTL_OFFSET);
+unsigned int setTimerControl(int timer_base, unsigned int enable, unsigned int mode, unsigned int clksel) {
+	unsigned int* timer_control_addr = (unsigned int*) (timer_base + CRTL_OFFSET);
 	DEBUG(DB_TIMER, "Timer3 base: 0x%x ctrl addr: 0x%x offset: 0x%x.\n", timer_base, timer_control_addr, CRTL_OFFSET);
 	
 	unsigned int control_value = (ENABLE_MASK & enable) | (MODE_MASK & mode) | (CLKSEL_MASK & clksel) ;
@@ -41,8 +39,8 @@ unsigned int setTimerControl(unsigned int* timer_base, unsigned int enable, unsi
 	return *timer_control_addr;
 }
 
-unsigned int getTimerValue(unsigned int* timer_base) {
-	unsigned int* timer_value_addr = (unsigned int*) ((unsigned int)timer_base + VAL_OFFSET);
+unsigned int getTimerValue(int timer_base) {
+	unsigned int* timer_value_addr = (unsigned int*) (timer_base + VAL_OFFSET);
 	unsigned int value = *timer_value_addr;
 	return value;
 }
@@ -51,28 +49,28 @@ unsigned int getTimerValue(unsigned int* timer_base) {
  * IO Methods
  */
 
-void printAsciControl( int channel, char *control, char *arg1, char *arg2 ) {
-	plprintf( channel, "%c[", ASCI_ESC);
-	if(arg1) plprintf( channel, "%s", arg1);
-	if(arg2) plprintf( channel, ";%s", arg2);
-	plprintf( channel, "%s", control );
+void printAsciControl(int channel, char *control, char *arg1, char *arg2) {
+	plprintf(channel, "%c[", ASCI_ESC);
+	if(arg1) plprintf(channel, "%s", arg1);
+	if(arg2) plprintf(channel, ";%s", arg2);
+	plprintf(channel, "%s", control);
 }
 
-void setRegisterBit( int base, int offset, int mask, int value ) {
-	int *addr = (int *)( base + offset );
+void setRegisterBit(int base, int offset, int mask, int value) {
+	int *addr = (int *)(base + offset);
 	int buf = *addr;
 	buf = value ? buf | mask : buf & ~mask;
 	*addr = buf;
 	return;
 }
 
-int getRegister( int base, int offset ) {
-	int *addr = (int *)( base + offset );
+int getRegister(int base, int offset) {
+	int *addr = (int *)(base + offset);
 	return *addr;
 }
 
-int getRegisterBit( int base, int offset, int mask ) {
-	int *addr = (int *)( base + offset );
+int getRegisterBit(int base, int offset, int mask) {
+	int *addr = (int *)(base + offset);
 	return (*addr) & mask;
 }
 
@@ -80,65 +78,57 @@ int getRegisterBit( int base, int offset, int mask ) {
  * Main Polling Loop
  */
 
-int main( int argc, char* argv[] ) {
+int main(int argc, char* argv[]) {
 	
 	/* Initialize Global Variables */
-	char buffer[CHANNEL_COUNT * OUTPUT_BUFFER_SIZE];
-	unsigned int buffer_send_index[CHANNEL_COUNT];
-	unsigned int buffer_save_index[CHANNEL_COUNT];
+	char plio_buffer[CHANNEL_COUNT * OUTPUT_BUFFER_SIZE];
+	unsigned int plio_send_index[CHANNEL_COUNT];
+	unsigned int plio_save_index[CHANNEL_COUNT];
 	char i2a_buffer[12];
+	dbflags = DB_USER_INPUT | DB_TRAIN_CTRL; // Debug Flags
 	
-	/* Initialize Polling IO */
-	dbflags = DB_USER_INPUT | DB_TRAIN_CTRL;
-	plbootstrap(buffer, buffer_send_index, buffer_save_index);
-	plsetfifo( COM2, OFF );
-	// plsetspeed( COM2, 115200 );
-	plsetfifo( COM1, OFF );
-	plsetspeed( COM1, 2400 );
+	/* Initialize IO: setup buffer; BOTH: turn off fifo; COM1: speed to 2400, enable stp2 */
+	plbootstrap(plio_buffer, plio_send_index, plio_save_index);
+	plsetfifo(COM2, OFF);
+	plsetfifo(COM1, OFF);
+	plsetspeed(COM1, 2400);
+	setRegisterBit(UART1_BASE, UART_LCRH_OFFSET, STP2_MASK, ONE);
 	
-	printAsciControl( COM2, ASCI_CLEAR_SCREEN, 0, 0 );
-	// Do not need to set PEN, already 0x60
-	setRegisterBit( UART1_BASE, UART_LCRH_OFFSET, STP2_MASK, ONE );
+	printAsciControl(COM2, ASCI_CLEAR_SCREEN, 0, 0);
 	
-	/* Verifiying COM1's Configuration */
-	printAsciControl( COM2, ASCI_CURSOR_TO, LINE_DEBUG, COLUMN_FIRST );
-	DEBUG( DB_TRAIN_CTRL, "LCRH: 0x%x\n", getRegister( UART1_BASE, UART_LCRH_OFFSET));
-	DEBUG( DB_TRAIN_CTRL, "LCRM: 0x%x\n", getRegister( UART1_BASE, UART_LCRM_OFFSET));
-	DEBUG( DB_TRAIN_CTRL, "LCRL: 0x%x\n", getRegister( UART1_BASE, UART_LCRL_OFFSET));
-	DEBUG( DB_TRAIN_CTRL, "CTRL: 0x%x\n", getRegister( UART1_BASE, UART_CTLR_OFFSET));
-	DEBUG( DB_TRAIN_CTRL, "FLAG: 0x%x\n", getRegister( UART1_BASE, UART_FLAG_OFFSET));
-	
-
-	DEBUG( DB_PLIO, "PLIO Initialized.\n" );
+	/* Verifiying COM1's Configuration: nothing when debug flag is turned off */
+	printAsciControl(COM2, ASCI_CURSOR_TO, LINE_DEBUG, COLUMN_FIRST);
+	DEBUG(DB_IO, "COM1 LCRH: 0x%x\n", getRegister(UART1_BASE, UART_LCRH_OFFSET)); // 0x68
+	DEBUG(DB_IO, "COM1 LCRM: 0x%x\n", getRegister(UART1_BASE, UART_LCRM_OFFSET)); // 0x0
+	DEBUG(DB_IO, "COM1 LCRL: 0x%x\n", getRegister(UART1_BASE, UART_LCRL_OFFSET)); // 0xbf
+	DEBUG(DB_IO, "COM1 CTRL: 0x%x\n", getRegister(UART1_BASE, UART_CTLR_OFFSET)); // 0x1
+	DEBUG(DB_IO, "COM1 FLAG: 0x%x\n", getRegister(UART1_BASE, UART_FLAG_OFFSET)); // 0x91
+	DEBUG(DB_IO, "IO Initialized.\n");
 	
 	/* Initialize Timer: Enable Timer3 with free running mode and 2kHz clock */
 	unsigned int previous_timer_value = 0;
 	unsigned int timer_tick_remained = 0;
 	unsigned int tenth_sec_elapsed = 0;
 	
-	setTimerControl((unsigned int*)TIMER3_BASE, ONE, ZERO, ZERO);
-	previous_timer_value = getTimerValue((unsigned int*)TIMER3_BASE);
+	setTimerControl(TIMER3_BASE, ONE, ZERO, ZERO);
+	previous_timer_value = getTimerValue(TIMER3_BASE);
 	DEBUG(DB_TIMER, "Timer3 value start with 0x%x.\n", previous_timer_value);
 	
 	/* Initialize User Input Buffer */
-	char user_input[1000];
+	char user_input_buffer[1000];
 	unsigned int user_input_size = 0;
-	user_input[user_input_size] = '\0';
+	char user_input_char;
+	user_input_buffer[user_input_size] = '\0';
 		
 	/* Polling loop */
 	while(1) {
-		/* 
-		 * Polling IO: 
-		 * Give it a chance to send out char 
-		 */
-		plsend( COM1 );
-		plsend( COM2 );
 		
-		/* 
-		 * Timer: 
-		 * Increase timer value
-		 */
-		unsigned int timer_value = getTimerValue((unsigned int*)TIMER3_BASE);
+		/* Polling IO: Give it a chance to send out char */
+		plsend(COM1);
+		plsend(COM2);
+		
+		/* Timer: Calculate and display time elapsed */
+		unsigned int timer_value = getTimerValue(TIMER3_BASE);
 		unsigned int time_elapsed = previous_timer_value - timer_value;
 		
 		// Fix time_elapsed when underflow
@@ -149,80 +139,72 @@ int main( int argc, char* argv[] ) {
 		// If time elapsed more than 1/10 sec
 		if(time_elapsed >= 200)
 		{
+			// Add elapsed time into remaining ticks, then convert to tenth-sec
 			timer_tick_remained += time_elapsed;
 			for(;timer_tick_remained >= 200; timer_tick_remained -= 200) tenth_sec_elapsed++;
 			
-			printAsciControl( COM2, ASCI_CURSOR_TO, LINE_ELAPSED_TIME, COLUMN_FIRST );
-			plprintf( COM2, "Time elapsed: %d:%d,%d, timer value: 0x%x\n", tenth_sec_elapsed / 600, (tenth_sec_elapsed % 600) / 10, tenth_sec_elapsed % 10, timer_value);
-			plui2a( user_input_size + 1, 10, i2a_buffer );
-			printAsciControl( COM2, ASCI_CURSOR_TO, LINE_USER_INPUT, i2a_buffer );
+			printAsciControl(COM2, ASCI_CURSOR_TO, LINE_ELAPSED_TIME, COLUMN_FIRST);
+			plprintf(COM2, "Time elapsed: %d:%d,%d, timer value: 0x%x\n", tenth_sec_elapsed / 600, (tenth_sec_elapsed % 600) / 10, tenth_sec_elapsed % 10, timer_value);
+			plui2a(user_input_size + 1, 10, i2a_buffer);
+			printAsciControl(COM2, ASCI_CURSOR_TO, LINE_USER_INPUT, i2a_buffer);
 			previous_timer_value = timer_value;
 		}
 		
-		// If has been running more than 10s, break;
-		// if(tenth_sec_elapsed > 100) {
-		// 	break;
-		// }
-		
-		/* 
-		 * Monitoring User Input: 
-		 * 
-		 */
-		char user_input_char;
-		if(plgetc( COM2, &user_input_char ) > 0 && user_input_size < 1000) {
+		/* User Input */
+		if(plgetc(COM2, &user_input_char) > 0 && user_input_size < 1000) {
 			
+			// Push or pop char from user_input_buffer
 			if(user_input_char == ASCI_BACKSPACE) {
 				user_input_size--;
-				user_input[user_input_size] = '\0';
+				user_input_buffer[user_input_size] = '\0';
 			}
 			else {
-				user_input[user_input_size] = user_input_char;
+				user_input_buffer[user_input_size] = user_input_char;
 				user_input_size++;
-				user_input[user_input_size] = '\0';
+				user_input_buffer[user_input_size] = '\0';
 			}
 			
-			// Flush Input Display	
-			printAsciControl( COM2, ASCI_CURSOR_TO, LINE_USER_INPUT, COLUMN_FIRST );
-			printAsciControl( COM2, ASCI_CLEAR_TO_EOL, 0, 0 );
-			
+			// If is EOL
 			if(user_input_char == '\n' || user_input_char == '\r') {
-				DEBUG(DB_USER_INPUT, "User Input: Reach EOL. Input Size %u, value %s", user_input_size, user_input);
+				DEBUG(DB_USER_INPUT, "User Input: Reach EOL. Input Size %u, value %s", user_input_size, user_input_buffer);
 				// If is q, quit
-				if(user_input_size == 2 && user_input[0] == 'q') {
+				if(user_input_size == 2 && user_input_buffer[0] == 'q') {
 					break;
 				}
 				
 				// If is r, start the train
-				if(user_input_size == 2 && user_input[0] == 'r') {
+				if(user_input_size == 2 && user_input_buffer[0] == 'r') {
 					DEBUG(DB_TRAIN_CTRL, "Sending start\n");
-					bwputc( COM1, 96 );
-					plputc( COM1, 96 );
+					plputc(COM1, 96);
 				}
 				
 				// If is s, stop the train
-				if(user_input_size == 2 && user_input[0] == 's') {
+				if(user_input_size == 2 && user_input_buffer[0] == 's') {
 					DEBUG(DB_TRAIN_CTRL, "Sending stop\n");
-					bwputc( COM1, 97 );
-					plputc( COM1, 97 );
+					plputc(COM1, 97);
 				}
 				
 				// Send to last command
-				printAsciControl( COM2, ASCI_CURSOR_TO, LINE_LAST_COMMAND, COLUMN_FIRST );
-				printAsciControl( COM2, ASCI_CLEAR_TO_EOL, 0, 0 );
-				plputstr( COM2, user_input );
+				printAsciControl(COM2, ASCI_CURSOR_TO, LINE_LAST_COMMAND, COLUMN_FIRST);
+				printAsciControl(COM2, ASCI_CLEAR_TO_EOL, 0, 0);
+				plputstr(COM2, user_input_buffer);
 				
 				// Reset input buffer
-				user_input[0] = '\0';
+				user_input_buffer[0] = '\0';
 				user_input_size = 0;
 			}
-			plputstr( COM2, user_input );
+			
+			// Refresh Input Display
+			printAsciControl(COM2, ASCI_CURSOR_TO, LINE_USER_INPUT, COLUMN_FIRST);
+			printAsciControl(COM2, ASCI_CLEAR_TO_EOL, 0, 0);
+			plputstr(COM2, user_input_buffer);
 		 }
 	}
 	
-	printAsciControl( COM2, ASCI_CURSOR_TO, LINE_BOTTOM, COLUMN_FIRST );
+	printAsciControl(COM2, ASCI_CURSOR_TO, LINE_BOTTOM, COLUMN_FIRST);
 	
-	plflush( COM1 );
-	plflush( COM2 );
+	plflush(COM1);
+	plflush(COM2);
 	
 	plstat();
 	return 0;
